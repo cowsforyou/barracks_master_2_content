672990ee87b245f9c343e8fcd26fe99c2742c640
Eugene Soo <20952179+eugenesoo@users.noreply.github.com>
2019-12-17 02:12:09 -0800

    Adds mouseover tooltip effect for resource view
    This adds a mouseover effect that displays a tooltip to describe the
    resources. It's gold and lumber at this point. Copy needs to be changed.
    See: https://i.imgur.com/jesOo0P.gifv
    
    Also starting to get bothered by the lack of type-checking, might switch
    over to typescript at some point..
    
    Signed-off-by: Eugene Soo <20952179+eugenesoo@users.noreply.github.com>

--- panorama/scripts/resource.js
+++ panorama/scripts/resource.js
@@ -1,15 +1,34 @@
 "use strict";
 
-function OnPlayerLumberChanged ( playerData ) {
+// Variables for resources mouseover title and description
+const toolTips = {
+	gold: {
+		toolTipUI: $("#ResourceGold"),
+		title: `Gold`,
+		toolTip: `This text is meant to be replaced`,
+	},
+	lumber: {
+		toolTipUI: $("#ResourceLumber"),
+		title: `Lumber`,
+		toolTip: `This text is meant to be replaced`,
+	},
+};
+
+function showToolTip(resourceType) {
+	const {toolTipUI, title, toolTip} = toolTips[resourceType];
+	$.DispatchEvent("DOTAShowTitleTextTooltip", toolTipUI, title, toolTip);
+}
+
+function OnPlayerLumberChanged(playerData) {
 	//$.Msg("Player "+iPlayerID+" Lumber: "+lumber);
 	$('#LumberText').text = playerData.lumber;
 }
 
 (function () {
-	GameEvents.Subscribe( "player_lumber_changed", OnPlayerLumberChanged );
+	GameEvents.Subscribe("player_lumber_changed", OnPlayerLumberChanged);
 })();
 
-(function UpdateGold () {
+(function UpdateGold() {
 	const playerGold = Players.GetGold(Players.GetLocalPlayer());
 	$('#GoldText').text = playerGold;
 	$.Schedule(0.1, UpdateGold);
