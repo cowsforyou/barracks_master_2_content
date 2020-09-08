f180fb52555d6ee6f2fd11b46b9e35578d0a76ca
Eugene Soo <20952179+eugenesoo@users.noreply.github.com>
2020-09-08 15:21:20 +0800

    Create hero selection layout Base hero selection layout and logic changes, status to be built
    Signed-off-by: Eugene Soo <20952179+eugenesoo@users.noreply.github.com>

--- panorama/scripts/hero_selection.js
+++ panorama/scripts/hero_selection.js
@@ -7,31 +7,65 @@
 
 //Define variables
 var playerPanels = {};
-var canEnter = false;
+let selectedHero = "";
+let clickedReady = false;
+
+//Panels
+const HudMain = $.GetContextPanel().GetParent().GetParent().GetParent();
+const Hud = HudMain.FindChildTraverse("lower_hud");
+const Timer = HudMain.FindChildTraverse("topbar");
+const Minimap = HudMain.FindChildTraverse("minimap_container");
+const Panel = HudMain.FindChildTraverse("stackable_side_panels");
+const Chat = HudMain.FindChildTraverse("HudChat");
+const PauseInfo = HudMain.FindChildTraverse("PausedInfo");
+const scoreBoardScreen = $.GetContextPanel()
+  .GetParent()
+  .GetParent()
+  .FindChildTraverse("CustomUIContainer_FlyoutScoreboard");
+const resourcePanelScreen = $.GetContextPanel()
+  .GetParent()
+  .FindChildTraverse("ResourceLumber")
+  .GetParent();
 
 //Subscribe to events
-GameEvents.Subscribe( "picking_done", OnPickingDone );
-GameEvents.Subscribe( "picking_time_update", OnTimeUpdate );
-GameEvents.Subscribe( "picking_player_pick", OnPlayerPicked );
+GameEvents.Subscribe("picking_done", OnPickingDone);
+GameEvents.Subscribe("picking_time_update", OnTimeUpdate);
+GameEvents.Subscribe("picking_player_pick", OnPlayerPicked);
 
 /* Event Handlers
 =========================================================================*/
 
 /* Picking phase is done, allow the player to enter the game */
-function OnPickingDone( data ) {
-	$("#EnterGameBtn").RemoveClass( "disabled" );
-	$("#EnterGameBtnTxt").text = "Enter Game";
-	canEnter = true;
+function OnPickingDone(data) {
+  $.Msg("Picking Done");
+
+  if (selectedHero.length === 0) {
+    const random = Math.round(Math.random());
+    if (random === 1) {
+      selectedHero = "npc_dota_hero_keeper_of_the_light";
+    } else {
+      selectedHero = "npc_dota_hero_nevermore";
+    }
+    SelectHero();
+  }
+
+  if (!clickedReady) {
+    GameEvents.SendCustomGameEventToServer("hero_selected", {
+      HeroName: selectedHero,
+    });
+  }
+
+  EnterGame();
 }
 
 /* Visual timer update */
-function OnTimeUpdate( data ) {
-	$("#TimerTxt").text = data.time;
+function OnTimeUpdate(data) {
+  $("#TimerTxt").text = data.time;
 }
 
 /* A player has picked a hero */
-function OnPlayerPicked( data ) {
-	PlayerPicked( data.PlayerID, data.HeroName );
+function OnPlayerPicked(data) {
+  PlayerPicked(data.PlayerID, data.HeroName);
 }
 
 /* Functionality
@@ -39,116 +73,187 @@ function OnPlayerPicked( data ) {
 
 /* Add an empty element for each player in the game (steam avatar plus space for hero portrait) */
 function LoadPlayers() {
-	//Get the players for both teams
-	var radiantPlayers = Game.GetPlayerIDsOnTeam( DOTATeam_t.DOTA_TEAM_GOODGUYS );
-	var direPlayers = Game.GetPlayerIDsOnTeam( DOTATeam_t.DOTA_TEAM_BADGUYS );
+  //Get the players for both teams
+  var radiantPlayers = Game.GetPlayerIDsOnTeam(DOTATeam_t.DOTA_TEAM_GOODGUYS);
+  var direPlayers = Game.GetPlayerIDsOnTeam(DOTATeam_t.DOTA_TEAM_BADGUYS);
 
-	//Assign radiant players
-	$.Each( radiantPlayers, function( player ) {
-		$.Msg('test', player)
-		var playerPanel = Modular.Spawn( "picking_player", $("#LeftPlayers") );
-		playerPanel.SetPlayer( player );
+  //Assign radiant players
+  $.Each(radiantPlayers, function (player) {
+    $.Msg("test", player);
+    var playerPanel = Modular.Spawn("picking_player", $("#LeftPlayers"));
+    playerPanel.SetPlayer(player);
 
-		//Save the panel for later
-		playerPanels[player] = playerPanel;
-	});
+    //Save the panel for later
+    playerPanels[player] = playerPanel;
+  });
 
-	//Assign dire players
-	$.Each( direPlayers, function( player ) {
-		var playerPanel = Modular.Spawn( "picking_player", $("#RightPlayers") );
-		playerPanel.SetPlayer( player );
-		playerPanel.SetIsRight( true );
+  //Assign dire players
+  $.Each(direPlayers, function (player) {
+    var playerPanel = Modular.Spawn("picking_player", $("#RightPlayers"));
+    playerPanel.SetPlayer(player);
+    playerPanel.SetIsRight(true);
 
-		//Save the panel for later
-		playerPanels[player] = playerPanel;
-	});
+    //Save the panel for later
+    playerPanels[player] = playerPanel;
+  });
 }
 
-/* A player has picked a hero, tell the player's panel a hero was picked,
- * show the hero was taken and if the player that picked is the local player
- * swap to the hero preview screen. */
-function PlayerPicked( player, hero ) {
-	//Update the player panel
-	playerPanels[player].SetHero( hero );
+/* A player has picked a hero, update the status screen. */
+function PlayerPicked(player, hero) {
+  //Update the player panel
+  playerPanels[player].SetHero(hero);
 
-	//Disable the hero button
-	$('#'+hero).AddClass( "taken" );
+  //Disable the hero button
+  $("#" + hero).AddClass("taken");
 
-	//Check if the pick was by the local player
-	if ( player == Players.GetLocalPlayer() ) {
-		SwitchToHeroPreview( hero );
-	}
+  //Check if the pick was by the local player
+  if (player == Players.GetLocalPlayer()) {
+    SwitchToHeroPreview(hero);
+  }
 }
 
-/* Switch the content of the screen to show the picked hero instead of the
- * pickable heroes. */
-function SwitchToHeroPreview( heroName ) {
-	var previewPanel = $.CreatePanel("Panel", $('#PostPickScreen'), "HeroPreview");
-	previewPanel.BLoadLayoutFromString('<root><Panel><DOTAScenePanel particleonly="false" style="width: 600px; height: 600px; opacity-mask: url(\'s2r://panorama/images/masks/softedge_box_png.vtex\');" unit="'+heroName+'" /> </Panel></root>', false, false );
+/* Show the picked hero animation. */
+function SwitchToHeroPreview(heroName) {
+  $("#HeroPreview") && $("#HeroPreview").DeleteAsync(0.0);
+  $("#BuilderInfo") && $("#BuilderInfo").DeleteAsync(0.0);
 
-	$('#PostPickScreen').MoveChildBefore( previewPanel, $("#EnterGameBtn") );
+  var previewPanel = $.CreatePanel(
+    "Panel",
+    $("#FactionSelector"),
+    "HeroPreview"
+  );
 
-	$('#PickList').style.visibility = 'collapse';
-	$('#PostPickScreen').style.visibility = 'visible';
+  var textPanel = $.CreatePanel(
+    "Label",
+    $("#CustomisationSelector"),
+    "BuilderInfo"
+  );
+
+  let factionNamePreview;
+  let factionDescriptionHash;
+
+  if (heroName === "npc_dota_hero_keeper_of_the_light") {
+    factionNamePreview = "Ling";
+    factionDescriptionHash="#bm_lore_ling";
+    selectedHero = "npc_dota_hero_keeper_of_the_light";
+  } else if (heroName === "npc_dota_hero_nevermore") {
+    factionNamePreview = "Xoya";
+    factionDescriptionHash="#bm_lore_xoo";
+    selectedHero = "npc_dota_hero_nevermore";
+  } else {
+    factionNamePreview = "Random";
+    factionDescriptionHash="#bm_lore_random";
+    const random = Math.round(Math.random());
+
+    if (random === 1) {
+      selectedHero = "npc_dota_hero_keeper_of_the_light";
+    } else {
+      selectedHero = "npc_dota_hero_nevermore";
+    }
+  }
+
+  textPanel.BLoadLayoutFromString(
+    '<root><Label text="' + factionDescriptionHash +'" /></root>',
+    false,
+    false
+  );
+  previewPanel.BLoadLayoutFromString(
+    '<root><Panel style="horizontal-align: center; flow-children: down;"><DOTAScenePanel particleonly="false" style="width: 330px; height: 330px; margin-top: 20px; opacity-mask: url(\'s2r://panorama/images/masks/softedge_box_png.vtex\');" unit="' +
+      heroName +
+      '" /><Label style="horizontal-align: center; vertical-align: center; margin-top: 20px;" text="' +
+      factionNamePreview +
+      '" /></Panel></root>',
+    false,
+    false
+  );
+  $("#FactionSelector").MoveChildAfter(previewPanel, $("#FactionIcon"));
+  $("#CustomisationSelector").MoveChildAfter(textPanel);
 }
 
 /* Select a hero, called when a player clicks a hero panel in the layout */
-function SelectHero( heroName ) {
-	//Send the pick to the server
-	GameEvents.SendCustomGameEventToServer( "hero_selected", { HeroName: heroName } );
+function SelectHero() {
+  if (selectedHero.length === 0) {
+    const random = Math.round(Math.random());
+    if (random === 1) {
+      selectedHero = "npc_dota_hero_keeper_of_the_light";
+    } else {
+      selectedHero = "npc_dota_hero_nevermore";
+    }
+  }
+  //Send the pick to the server
+  GameEvents.SendCustomGameEventToServer("hero_selected", {
+    HeroName: selectedHero,
+  });
+
+  $("#ReadyBtn") && $("#ReadyBtn").AddClass("disabled");
+  $("#ReadyBtnTxt").text = "Waiting for others";
 }
 
 /* Enter the game by removing the picking screen, called when the player
  * clicks a button in the layout. */
 function EnterGame() {
-	if ( canEnter ) {
-		$('#PickingScreen').DeleteAsync( 0.0 );
-		var scoreBoardScreen = $.GetContextPanel().GetParent().GetParent().FindChildTraverse('CustomUIContainer_FlyoutScoreboard');
-		scoreBoardScreen.style.opacity = 1;
+  $.GetContextPanel().FindChildTraverse("SelectionContainer").DeleteAsync(0.0);
+  $.GetContextPanel().FindChildTraverse("StatusContainer").DeleteAsync(0.0);
+  $.GetContextPanel().FindChildTraverse("GameInfoContainer").DeleteAsync(0.0);
 
-		var Hud = $.GetContextPanel().GetParent().GetParent().GetParent().FindChildTraverse('HUDElements');
-		Hud.style.visibility = 'visible';
+  //Show score board overlay
+  scoreBoardScreen.style.opacity = 1;
 
-		var resourcePanelScreen = $.GetContextPanel().GetParent().FindChildTraverse('ResourceLumber').GetParent();
-		resourcePanelScreen.style.opacity = 1;
+  //Show resource panel
+  resourcePanelScreen.style.opacity = 1;
 
-		var resourcePanelScreen = $.GetContextPanel().GetParent().FindChildTraverse('ResourceLumber').GetParent();
-		resourcePanelScreen.style.opacity = 1;
+  //Show HUD elements
+  Hud.style.opacity = 1;
 
-		var PauseInfo = $.GetContextPanel().GetParent().GetParent().GetParent().FindChildTraverse('PausedInfo');
-		PauseInfo.style.opacity = 1;
+  //Show Timer
+  Timer.style.opacity = 1;
 
-		GameEvents.SendCustomGameEventToServer( "set_player_color_unselected", {});
-	}
+  //Show Minimap
+  Minimap.style.opacity = 1;
+
+  //Show Side Panels
+  Panel.style.opacity = 1;
+
+  //Reposition back Chat Bar
+  Chat.style.y = "-240px";
+
+  //Show Pause Game element
+  PauseInfo.style.opacity = 1;
+
+  GameEvents.SendCustomGameEventToServer("set_player_color_unselected", {});
 }
 
 function SelectColor(color) {
-	GameEvents.SendCustomGameEventToServer( "set_player_color", { "color" : color } );
+  GameEvents.SendCustomGameEventToServer("set_player_color", { color: color });
 }
 
 /* Initialisation - runs when the element is created
 =========================================================================*/
 (function () {
-	//Set panel visibility
-	$('#PickList').style.visibility = 'visible';
-	$('#PostPickScreen').style.visibility = 'collapse';
-	
-	//Hide scoreboard screen
-	var scoreBoardScreen = $.GetContextPanel().GetParent().GetParent().FindChildTraverse('CustomUIContainer_FlyoutScoreboard');
-	scoreBoardScreen.style.opacity = 0;
+  //Hide scoreboard screen
+  scoreBoardScreen.style.opacity = 0;
 
-	//Hide resource panel
-	var resourcePanelScreen = $.GetContextPanel().GetParent().FindChildTraverse('ResourceLumber').GetParent();
-	resourcePanelScreen.style.opacity = 0;
+  //Hide resource panel
+  resourcePanelScreen.style.opacity = 0;
 
-	//Hide HUD elements
-	var Hud = $.GetContextPanel().GetParent().GetParent().GetParent().FindChildTraverse('HUDElements');
-	Hud.style.visibility = 'collapse';
+  //Hide HUD elements
+  Hud.style.opacity = 0;
 
-	//Hide Pause Game element
-	var PauseInfo = $.GetContextPanel().GetParent().GetParent().GetParent().FindChildTraverse('PausedInfo');
-	PauseInfo.style.opacity = 0;
+  //Hide Timer
+  Timer.style.opacity = 0;
 
-	///Load player elements
-	LoadPlayers();
+  //Hide Minimap
+  Minimap.style.opacity = 0;
+
+  //Hide Side Panels
+  Panel.style.opacity = 0;
+
+  //Reposition Chat Bar
+  Chat.style.y = "-20px";
+
+  //Hide Pause Game element
+  PauseInfo.style.opacity = 0;
+
+  ///Load player elements
+  LoadPlayers();
 })();
