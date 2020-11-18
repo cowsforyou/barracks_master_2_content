32dc84626cca1d76212507650bf7641dc2dca216
Eugene Soo <20952179+eugenesoo@users.noreply.github.com>
2020-11-18 17:46:47 +0800

    Potential fix for leaderboard
    Signed-off-by: Eugene Soo <20952179+eugenesoo@users.noreply.github.com>

--- panorama/scripts/custom_loading_screen.js
+++ panorama/scripts/custom_loading_screen.js
@@ -10,6 +10,11 @@ function OnLeaderBoardUpdate(data) {
 
 for (let index = 0; index < results.length; index++) {
     const entry = results[index]; 
+    // file://{resources}/layout/custom_game/modules/leaderboard_entry.xml
+    // Why are we adding in a comment here with the file path?
+    // modular.js load a layout referring to a dynamic file pathing ( MODULE_BASE_PATH + name + ".xml") but resourcecompiler finds xml files to compile using a simple regexp-like match
+    // If we want to refer to layouts dynamically, at the very least we have to reference a static file path somehow (in any file, even in comment)
+    // thanks to ark120202 on ModDota for the insight.
     var leaderboardEntryPanel = Modular.Spawn("leaderboard_entry", $("#BM_Ladder"));
     leaderboardEntryPanel.SetPlayer(entry.objectId, entry.bmPoints);
 }
